28159121289839d556d5f2bc8c1b4d7540946739
solovyovsdom <joelborofsky@mac.com>
2023-10-09 11:53:56 -0500

    created stardew valley page

---
+++ app/reviews/stardew-valley/page.jsx
@@ -0,0 +1,11 @@
+export default function StardewValleyPage() {
+  return (
+    <>
+      <h1>Stardew Valley</h1>
+      <p>
+        Stardew Valley is a farming simulation game. You inherit a farm from
+        your grandfather and move to a small town to start a new life.
+      </p>
+    </>
+  )
+}
