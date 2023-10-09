40e90b1258faa46fa0f8ff8527964db22b53672a
solovyovsdom <joelborofsky@mac.com>
2023-10-09 11:57:26 -0500

    added about and review route

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Indie Gamer is a site dedicated to reviewing the best indie games.</p>
+    </>
+  )
+}
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+    return (
+        <>
+            <h1>Hollow Knight</h1>
+            <p>One of the best games ever made.</p>
+        </>
+    )
+}
